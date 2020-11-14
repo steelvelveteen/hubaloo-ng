70322239fb9791aba6f0f9b20a10fd1c6909c652
steelvelveteen <joeyvico@gmail.com>
2020-11-14 10:48:04 +1030

    Added logout function and isLoggedIn function check

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -28,4 +28,12 @@ export class AuthService {
   constructor(private dataService: DataService) { }
 
   Login = (credentials: CredentialsType): Observable<LoginResponseType> => this.dataService.Post(credentials, '/auth/login');
+
+  isLoggedIn = (): boolean => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    return !!token;
+  }
+
+  logout = (): void =>
+    localStorage.removeItem('token')
 }
